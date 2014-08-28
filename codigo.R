2f333cdf0de8ba1b4d0682f8097c9d816e1417cd
Luis Villalobos <luised03@yahoo.com>
2014-08-28 10:58:00 -0500

    Update codigo.R

--- codigo.R
+++ codigo.R
@@ -1,12 +1,19 @@
 T=1000
 x=0:T
 y=T-x
-lines(x,y, type="l", col=3)
+plot(x,y, type="l", col=3)
  
 
 
 alpha=.5
 beta=.3
 
-plot(x,x^alpha, type="l", col="blue3")
+plot(x,x^alpha, type="l", col="blue3", xlim=)
 lines(x,x^beta, col="blue2")
+
+
+k1=20
+y2=(k1-x^alpha)^(1/beta)
+
+plot(x,y, type="l", col=3)
+lines(x,y2, col="blue")
