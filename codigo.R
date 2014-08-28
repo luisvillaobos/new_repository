8bd674fe372313338159657f8c83859ea60b20c6
Luis Villalobos <luised03@yahoo.com>
2014-08-28 11:16:36 -0500

    Update codigo.R

--- codigo.R
+++ codigo.R
@@ -6,14 +6,20 @@ plot(x,y, type="l", col=3)
 
 
 alpha=.5
-beta=.3
+beta=.5
 
 plot(x,x^alpha, type="l", col="blue3", xlim=)
 lines(x,x^beta, col="blue2")
 
 
-k1=20
+k1=44
 y2=(k1-x^alpha)^(1/beta)
 
 plot(x,y, type="l", col=3)
-lines(x,y2, col="blue")
+lines(x,y2, col="red")
+
+
+
+valor=function(x){
+  -(x^alpha+(T-x)^beta)  
+}
